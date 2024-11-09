d621510000fd8e8896d6fa6c898489af29082bef
Andrew Kenneally <19553807+andrewk10@users.noreply.github.com>
2024-11-09 16:27:07 +0000

    Added gnome-disk-utility
    And cleaned up build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,5 @@ RELEASE="$(rpm -E %fedora)"
 
 
 ### Install packages
+rpm-ostree install clamav lynis gnome-disk-utility
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-rpm-ostree install clamav lynis
-
-#### Example for enabling a System Unit File
-# systemctl enable podman.socket
